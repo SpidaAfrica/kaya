ad15b34c737ebc79929461c57fbe6de1fc2230f2
SpidaAfrica <spidaafrica@gmail.com>
2025-04-17 09:47:24 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Add other Next.js config options here
 };
 
 export default nextConfig;
 
+// If you want to enable PWA later:
+// import withPWA from 'next-pwa'
 // export default withPWA({
 //   dest: "public",
 //   disable: process.env.NODE_ENV === "development",
